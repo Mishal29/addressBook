3e5006d42e6abc4082298bcbcf191eef8eca62fe
mishal29 <h3500g@icloud.com>
2022-06-06 00:52:36 +0900

    mask sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@
 INSERT INTO user VALUES('hoge', 'hoge', 'hoge');
 
 --住所
-INSERT INTO address VALUES(1, '小林 達也', 'こばやし たつや', '富山県館山市本町3-2-8', '03823677184', 'kobayashi@morijyobi.ac.jp', 'サンプル', 'hoge');
+INSERT INTO address VALUES(1, '小林 達也', 'こばやし たつや', '富山県館山市本町3-2-8', '03823677184', 'kobayashi@example.jp', 'サンプル', 'hoge');
 
-INSERT INTO address VALUES(2, '戸屋 恵実子', 'とや えみこ', '福島県里土村11-6', '12345678901', 'toya@morijyobi.ac.jp', 'サンプル２', 'hoge');
+INSERT INTO address VALUES(2, '戸屋 恵実子', 'とや えみこ', '福島県里土村11-6', '12345678901', 'toya@example.jp', 'サンプル２', 'hoge');
